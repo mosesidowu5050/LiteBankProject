93710b4c79e676c6a43bcab86ce6ef01efbee15e
idowu moses <dynamopraize5050@gmail.com>
2025-08-23 10:17:41 +0100

    update code to branch

--- src/test/java/com/litebank/controller/AccountControllerTest.java
+++ src/test/java/com/litebank/controller/AccountControllerTest.java
@@ -36,6 +36,7 @@ class AccountControllerTest {
         String json = mapper.writeValueAsString(depositRequest);
 
         String depositEndpoint = "/api/v1/account/deposit";
+
         mockMvc.perform(MockMvcRequestBuilders.post(depositEndpoint)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(json))
